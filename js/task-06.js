1140a5235ba91d14f1f2cd36648c7361f7321a5d
Roman <roman.baytala@gmail.com>
2022-09-11 22:22:33 +0300

    hw1

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,13 @@
 const refs = {
-    validInputRef: document.querySelector('#validation-input')
+    validInputRef: document.querySelector('#validation-input'),
 }
+const inputLen = refs.validInputRef.getAttribute("data-length")
 refs.validInputRef.addEventListener('blur', onTargetBtnEvent);
 function onTargetBtnEvent(event) {
-    const inputValEl = event.currentTarget.value
-    if (inputValEl.length < refs.validInputRef.dataset.length || 
-        inputValEl.length > refs.validInputRef.dataset.length) {
-        refs.validInputRef.classList = 'invalid'
-    } else {
+    const inputValEl = event.currentTarget.value.length === Number(refs.inputLen)
+    if (inputValEl) {
         refs.validInputRef.classList = 'valid'
+    } else {
+        refs.validInputRef.classList = 'invalid'
     }
 }
